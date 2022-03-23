8a0241839b8acbdf0824fd39441c84bc502b0230
Morozov Vyacheslav <morozov_vyacheslav@smartworld.team>
2022-03-23 21:27:32 +0700

    Add exception filter

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,40 @@ import express, { Express } from 'express';
 import { PORT } from './constants/constants.js';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service.js';
+import { UserController } from './users/users.controller.js';
+import { ExceptionFilter } from './errors/exception.filter.js';
 
 class App {
   app: Express;
   server: Server;
   port: number;
   logger: LoggerService;
+  userController: UserController;
+  exceptionFilter: ExceptionFilter;
 
-  constructor(logger: LoggerService) {
+  constructor(
+    logger: LoggerService,
+    userController: UserController,
+    exceptionFilter: ExceptionFilter
+  ) {
     this.app = express();
     this.port = PORT;
     this.logger = logger;
+    this.userController = userController;
+    this.exceptionFilter = exceptionFilter;
   }
 
-  useRoutes() {}
+  useRoutes() {
+    this.app.use('/users', this.userController.router);
+  }
+
+  useExceptionFilters() {
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+  }
 
   public async init() {
     this.useRoutes();
+    this.useExceptionFilters();
     this.server = this.app.listen(this.port);
     this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { App } from './app.js';
 import { LoggerService } from './logger/logger.service.js';
+import { UserController } from './users/users.controller.js';
+import { ExceptionFilter } from './errors/exception.filter.js';
 
 const bootstrap = async () => {
-  const app = new App(new LoggerService());
+  const logger = new LoggerService();
+  const app = new App(
+    logger,
+    new UserController(logger),
+    new ExceptionFilter(logger)
+  );
   await app.init();
 };
 
